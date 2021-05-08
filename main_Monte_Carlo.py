f4b1fb8f538dc086acdcf8ff5ca794a2f46380c0
alpertSnow <rmddxx@163.com>
2021-05-08 16:46:52 +0800

    更新了MC随机采样算法

--- main_Monte_Carlo.py
+++ main_Monte_Carlo.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import warnings
 from tqdm import tqdm  # 进度条
 from multiprocessing import Pool
 from functools import partial
 from ClassDef import Compete  # 竞争类板块对象
 from ClassDef import Public  # 公共服务类板块对象
+from ClassDef import Special  # 特殊功能类类板块对象（此处用于壳公司与抵消对象）
 from ClassDef import SpecialGov  # 特殊功能类类板块对象（政府型）
 from ClassDef import SpecialMarket  # 特殊功能类类板块对象（经营型）
 from FnDef import randomize_inputs  # 根据输入的参数，生成一个随机的列表
@@ -20,6 +22,7 @@ from ConstVar import SAMPLE_SIZE  # 随机采样个数
 from ConstVar import THREAD_NO  # 线程数
 
 pd.options.mode.chained_assignment = None  # default='warn'
+warnings.filterwarnings('ignore', category=RuntimeWarning)
 
 """Unit: 万元，人"""
 
@@ -36,55 +39,39 @@ def do(thread, df1, df2, df3, df4):
         for i, data in inputs.iterrows():
             class_name = data['category'] + data['subcategory']
             exec(data['var_name'] + "=" + class_name + """(var_name=data['var_name'], name=data['name'], \
-                     package_last_year=data['package_last_year'], \
-                     total_profit_last_year=data['total_profit_last_year'], \
-                     total_profit_last_year_elimination=data['total_profit_last_year_elimination'], \
-                     patmi_self_last_year=data['patmi_self_last_year'], \
-                     patmi_self_last_year_elimination=data['patmi_self_last_year_elimination'], \
-                     revenue_last_year=data['revenue_last_year'], \
-                     revenue_last_year_elimination=data['revenue_last_year_elimination'], \
-                     cost_last_year=data['cost_last_year'], \
-                     cost_last_year_elimination=data['cost_last_year_elimination'], \
-                     invest_income_last_year=data['invest_income_last_year'], \
-                     other_income_last_year=data['other_income_last_year'], \
-                     total_profit=data['total_profit'], \
-                     total_profit_elimination=data['total_profit_elimination'], \
-                     patmi_self=data['patmi_self'], \
-                     patmi_elimination=data['patmi_elimination'], \
-                     revenue=data['revenue'], \
-                     revenue_elimination=data['revenue_elimination'], \
-                     cost=data['cost'], \
-                     cost_elimination=data['cost_elimination'], \
-                     invest_income=data['invest_income'], \
-                     other_income=data['other_income'], \
-                     avg_employee_last_year=data['avg_employee_last_year'], \
-                     avg_employee=data['avg_employee'], \
-                     key_score=data['key_score'], \
-                     eff_index_1_name=data['eff_index_1_name'], \
-                     eff_index_2_name=data['eff_index_2_name'], \
-                     eff_index_3_name=data['eff_index_3_name'], \
-                     eff_index_4_name=data['eff_index_4_name'], \
-                     eff_index_1_last_year=data['eff_index_1_last_year'], \
-                     eff_index_2_last_year=data['eff_index_2_last_year'], \
-                     eff_index_3_last_year=data['eff_index_3_last_year'], \
-                     eff_index_4_last_year=data['eff_index_4_last_year'], \
-                     eff_index_1=data['eff_index_1'], \
-                     eff_index_2=data['eff_index_2'], \
-                     eff_index_3=data['eff_index_3'], \
-                     eff_index_4=data['eff_index_4'], \
-                     eff_index_1_weight=data['eff_index_1_weight'], \
-                     eff_index_2_weight=data['eff_index_2_weight'], \
-                     eff_index_3_weight=data['eff_index_3_weight'], \
-                     eff_index_4_weight=data['eff_index_4_weight'], \
-                     quality_index_last_year=data['quality_index_last_year'], \
-                     cost_index_last_year=data['cost_index_last_year'], \
-                     operate_index_last_year=data['operate_index_last_year'], \
-                     quality_index=data['quality_index'], \
-                     cost_index=data['cost_index'], \
-                     operate_index=data['operate_index'], \
-                     load_index_last_year=data['load_index_last_year'], \
-                     load_index=data['load_index'], \
-                     financial_index_name=data['financial_index_name'])""")
+                         package_last_year=data['package_last_year'], \
+                         defer_last_year=data['defer_last_year'], \
+                         distributable_last_year=data['distributable_last_year'], \
+                         total_profit_last_year=data['total_profit_last_year'], \
+                         patmi_self_last_year=data['patmi_self_last_year'], \
+                         patmi_BII_last_year=data['patmi_BII_last_year'], \
+                         revenue_last_year=data['revenue_last_year'], \
+                         cost_last_year=data['cost_last_year'], \
+                         invest_income_last_year=data['invest_income_last_year'], \
+                         other_income_last_year=data['other_income_last_year'], \
+                         labor_cost_last_year=data['labor_cost_last_year'], \
+                         total_profit=data['total_profit'], \
+                         patmi_self=data['patmi_self'], \
+                         patmi_BII=data['patmi_BII'], \
+                         revenue=data['revenue'], \
+                         cost=data['cost'], \
+                         invest_income=data['invest_income'], \
+                         other_income=data['other_income'], \
+                         labor_cost=data['labor_cost'], \
+                         avg_employee_last_year=data['avg_employee_last_year'], \
+                         avg_employee=data['avg_employee'], \
+                         key_score=data['key_score'], \
+                         eff_index_1_weight=data['eff_index_1_weight'], \
+                         eff_index_2_weight=data['eff_index_2_weight'], \
+                         eff_index_3_weight=data['eff_index_3_weight'], \
+                         eff_index_4_weight=data['eff_index_4_weight'], \
+                         quality_index_last_year=data['quality_index_last_year'], \
+                         cost_index_last_year=data['cost_index_last_year'], \
+                         operate_index_last_year=data['operate_index_last_year'], \
+                         quality_index=data['quality_index'], \
+                         cost_index=data['cost_index'], \
+                         operate_index=data['operate_index'], \
+                         financial_index_name=data['financial_index_name'])""")
 
         # 创建单位实例的列表
         all_var_name_string = ','.join(list(inputs['var_name']))
@@ -108,7 +95,6 @@ def do(thread, df1, df2, df3, df4):
         results_df = pd.DataFrame()
 
         # 计算板块国资委批复额度
-        warned = [0] * len(section_names)
         for i in range(len(section_names)):
             # 摘出各板块单位的表格
             section_df_list.append(units_df[units_df["category"] == section_names[i]])
@@ -126,7 +112,7 @@ def do(thread, df1, df2, df3, df4):
             section_total_package_3_list.append(section_df_list[i].total_package_3.sum())
 
             # 微调平账
-            section_df_list[i] = tune(section_df_list[i], section_total_package_3_list[i],
+            section_df_list[i] = tune(section_df_list[i],
                                       approved_obj_list[i].package_final, approved_obj_list[i].defer_final)
             # 创建版块合计对象
             section_obj_list.append(create_section_obj(section_df_list[i]))
@@ -162,8 +148,8 @@ if __name__ == '__main__':
     pool = Pool(THREAD_NO)  # Create a multiprocessing Pool
     all_results_df = pd.concat(pool.map(partial_do, range(THREAD_NO)))  # process data_inputs iterable with pool
     """输出csv"""
-    print("采样完成，开始输出！")
-    all_results_df.to_csv("results.csv", encoding="UTF-8", float_format='%.5f', index=False)
+    print("\n采样完成，开始输出！")
+    all_results_df.to_csv("results-MC.csv", encoding="UTF-8", float_format='%.5f', index=False)
     # all_results_df.to_excel("results.xlsx", sheet_name="all", encoding="UTF-8", engine='xlsxwriter', float_format='%.5f'
     # , index=False)
     print("输出完成，程序结束！")
